0e4ec4c3f637c22ce30ae9c2fb8e5bba94a3b7c2
DoloresPGz <dolores.pgz@gmail.com>
2023-10-18 21:17:47 -0600

    nodemon y dotenv funcionando

---
+++ npm-ejercicios/ejemplo-2/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+console.log("Hola desde mi index.js")
+console.log("Este es otro conole.log")
+console.log("Mi primer cambio con nodemon")
+
+
+/*for(let i = 0;i < 10; i++){
+    console.log("Estoy loco")
+}*/
+
+console.log(process.env.SECRET_KEY)
